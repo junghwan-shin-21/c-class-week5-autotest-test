765ed024db498a642ba759c580eb0df127fb5811
junghwan-shin-21 <junghwan.shin@ssu.ac.kr>
2025-09-15 13:11:01 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# C-Class-3주차-Repo
+- git system 연습
+- 연산자, 자료형 연습
---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    int x = 20;
+    int y = 100;
+
+    printf("결과: %d\n", (x << 2) | y);
+
+    return 0;
+}
+
